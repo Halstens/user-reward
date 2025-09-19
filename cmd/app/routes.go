9c07e72fe3daf5dde0e5743c6378714282218852
PavelAtiev <57226034+Royselect@users.noreply.github.com>
2025-09-19 15:55:02 +0300

    docker and docker-compose is fixed

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -6,7 +6,7 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/users/{id}/status", app.ShowUserInfo)
 	mux.HandleFunc("/users/leaderboard", app.ShowTopUserByBalance)
-	mux.HandleFunc("/users/{id}/task/complete ", app.CompletedTask)
+	mux.HandleFunc("/users/{id}/task/complete", app.CompletedTask)
 	mux.HandleFunc("/users/{id}/referrer", app.AddRefferer)
 
 	return mux
